4bcb794cc79242f606dbbc40c4ef786085f4da1d
anr <naveenreddyaleti@gmail.com>
2016-12-27 22:44:24 +0530

    added all fiels to repo

---
+++ src/XYZ/FirstDemo.java
@@ -0,0 +1,11 @@
+package XYZ;
+import org.testng.annotations.Test;
+
+
+public class FirstDemo {
+
+	@Test
+	public void helloTest(){
+		System.out.println("hi..well come to the jenkins world");	
+	}
+}
